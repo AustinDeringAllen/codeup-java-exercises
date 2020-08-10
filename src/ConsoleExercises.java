92862bb9a77f19db8b60923c15e845e752880d8b
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-08-10 11:13:31 -0500

    Console Exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        final double pi = 3.14159;
+
+        System.out.printf("The approximate value of pi is %.4s.\n",pi);
+
+        // SECOND EXERCISE
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an Integer: ");
+
+        boolean integer = true;
+
+        try {
+            int userInput = scanner.nextInt();
+        } catch(Exception error) {
+            System.out.println("" + error);
+            integer = false;
+        }
+
+        if(integer) {
+            System.out.println("Congratulations on entering an Integer");
+        } else {
+            System.out.println("I told you to enter an Integer...");
+        }
+
+        String wordOne = "", wordTwo = "", wordThree = "";
+        System.out.println("Please enter 3 words");
+        try {
+            wordOne = scanner.next();
+            wordTwo = scanner.next();
+            wordThree = scanner.next();
+        } catch(Exception error) {
+            System.out.println("" + error);
+        }
+        System.out.printf("First Word: %s\nSecond Word: %s\nThird Word: %s", wordOne, wordTwo, wordThree);
+        scanner.nextLine();
+
+        String userSentence = "";
+        try {
+            System.out.println("\n\nPlease Enter a sentence: ");
+            userSentence = scanner.nextLine();
+        } catch(Exception error) {
+            System.out.println("" + error);
+        }
+
+        System.out.printf("The sentence entered is: \n%s", userSentence);
+
+        // THIRD EXERCISE
+        int length = 0, width = 0;
+        System.out.println("\n\nPlease enter a length and a width: ");
+        try {
+            length = scanner.nextInt();
+            width = scanner.nextInt();
+        } catch(Exception error) {
+            System.out.println("" + error);
+        }
+
+        int perimeter = length + length + width + width;
+        int area = length * width;
+
+        System.out.printf("The perimeter is: %s\nThe area is: %s", perimeter, area);
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,42 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // sout -> tab
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        //String myString = 3.14159;
+        String myString = "Austin Dering-Allen";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+        int y = 5;
+        System.out.println(++y); // Pre incrementing will display the new X
+        System.out.println(y);
+
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;
+        //int three = (int) "three";
+
+        //int x = 4;
+        //x = x + 5;
+        //x += 5;
+
+        //int x = 3;
+        //int y = 4;
+        //y = y * x;
+        // y *= x;
+
+        //int x = 10;
+        //int y = 2;
+        //x = x / y;
+        // x /= y;
+        //y = y - x;
+        // y -= x;
+
+        System.out.println(Integer.MAX_VALUE + 1);
+    }
+}
