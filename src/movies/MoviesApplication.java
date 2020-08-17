379583097a1f485948386b45cc1b020275bec45c
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-08-17 09:59:14 -0500

    Made the Arrays exercise MoviesApplication dynamic

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -2,27 +2,45 @@ package movies;
 
 import util.Input;
 
+import java.util.Arrays;
+
 public class MoviesApplication {
-    private static Movie[] movies;
+    private static Movie[] movies = MoviesArray.findAll();
+    private static String[] categories;
+
+    public static void getCategories() {
+        StringBuilder string = new StringBuilder();
+
+        for(Movie movie : movies) {
+            String category = movie.getCategory();
+            if(!string.toString().contains(category)) {
+                string.append(category);
+                string.append(" ");
+            }
+        }
+
+        categories = string.toString().split(" ");
+        System.out.println(Arrays.toString(categories));
+    }
 
     public static void main(String[] args) {
+        getCategories();
         menu();
     }
 
     public static void menu() {
         Input input = new Input();
 
-        System.out.println("What would you like to do? \n");
+        System.out.println("What would you like to do?\n");
         System.out.println("0 - exit");
-        System.out.println("1 - view all movies");
-        System.out.println("2 - view movies in the animated category");
-        System.out.println("3 - view movies in the drama category");
-        System.out.println("4 - view movies in the horror category");
-        System.out.println("5 - view movies in the musical category");
-        System.out.println("6 - view movies in the comedy category");
+        System.out.println("1 - add a new movie");
+        System.out.println("2 - view all movies");
+        for(int i=0; i<categories.length; i++) {
+            System.out.printf("%s - view movies in the %s category\n",i+3,categories[i]);
+        }
 
         System.out.print("\nEnter your choice: ");
-        int userInput = input.getInt(0,6);
+        int userInput = input.getInt(0,categories.length+3);
         filterMovies(userInput);
     }
 
@@ -31,29 +49,18 @@ public class MoviesApplication {
             case 0:
                 return;
             case 1:
-                displayMovies();
+                addMovie();
                 break;
             case 2:
-                displayMovies("animated");
+                displayMovies();
                 break;
-            case 3:
-                displayMovies("drama");
-                break;
-            case 4:
-                displayMovies("horror");
-                break;
-            case 5:
-                displayMovies("musical");
-                break;
-            case 6:
-                displayMovies("comedy");
+            default:
+                displayMovies(categories[option-3]);
                 break;
         }
     }
 
     public static void displayMovies() {
-        Movie[] movies = MoviesArray.findAll();
-
         System.out.println();
         for(Movie movie : movies) {
             System.out.printf("%s -- %s \n",movie.getName(),movie.getCategory());
@@ -63,14 +70,29 @@ public class MoviesApplication {
     }
 
     public static void displayMovies(String type) {
-        Movie[] movies = MoviesArray.findAll();
-
         System.out.println();
         for(Movie movie : movies) {
             if(movie.getCategory().equals(type))
                 System.out.printf("%s -- %s \n",movie.getName(),movie.getCategory());
         }
         System.out.println();
+        menu();
+    }
+
+    public static void addMovie() {
+        Input input = new Input();
+        Movie[] temp = new Movie[movies.length + 1];
+        System.arraycopy(movies, 0, temp, 0, movies.length);
+
+        System.out.print("Enter a movie name: ");
+        String name = input.getString();
+        System.out.print("Enter a movie category: ");
+        String category = input.getString();
+
+
+        temp[temp.length-1] = new Movie(name, category.toLowerCase());
+        movies = temp;
+        getCategories();
         menu();
     }
 }
