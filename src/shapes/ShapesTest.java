81fba84618cf0257657a80b9f097181215db38e7
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-08-18 14:14:53 -0500

    Interfaces and Abstract Exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,21 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5,4);
-        System.out.println(box1.getPerimeter()); // 18
-        System.out.println(box1.getArea()); // 20
+//        Rectangle box1 = new Rectangle(5,4);
+//        System.out.println(box1.getPerimeter()); // 18
+//        System.out.println(box1.getArea()); // 20
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter()); // 20
+//        System.out.println(box2.getArea()); // 25
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter()); // 20
-        System.out.println(box2.getArea()); // 25
+        Measurable myShape;
+        myShape = new Rectangle(7,3);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
+
+        myShape = new Square(5);
+        System.out.println(myShape.getArea());
+        System.out.println(myShape.getPerimeter());
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,18 +1,28 @@
 package shapes;
 
-public class Square extends Rectangle{
-    private int side;
+public class Square extends Quadrilateral {
 
     public Square(int side) {
         super(side, side);
-        this.side = side;
     }
 
-    public int getArea() {
-        return 4 * side;
+    @Override
+    public void setLength(int length) {
+        this.length = length;
     }
 
-    public int getPerimeter() {
-        return (int) Math.pow(side,2);
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * getLength() + 2 * getWidth();
+    }
+
+    @Override
+    public double getArea() {
+        return getLength() * getWidth();
     }
 }
